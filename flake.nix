30edb6c71add054fca48b352a3b5472ebcd710e6
Robin Appelman <robin@icewind.nl>
2023-11-24 23:39:55 +0100

    flake build

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
     utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/release-22.11";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    npmlock2nix.url = "github:nix-community/npmlock2nix";
+    npmlock2nix.flake = false;
   };
 
   outputs = {
@@ -10,19 +14,49 @@
     nixpkgs,
     utils,
     rust-overlay,
+    naersk,
+    npmlock2nix,
   }:
-  utils.lib.eachDefaultSystem (system: let
-      overlays = [ (import rust-overlay) ];
+    utils.lib.eachDefaultSystem (system: let
+      overlays = [
+        (import rust-overlay)
+        (final: prev: {
+          npmlock2nix = import npmlock2nix {pkgs = final;};
+        })
+      ];
       pkgs = (import nixpkgs) {
         inherit system overlays;
       };
+      inherit (pkgs) lib callPackage;
+
+      src = lib.sources.sourceByRegex ./. ["Cargo.*" "(src|build|images|script|style)(/.*)?" "sqlx-data.json"];
+      nodeSrc = lib.sources.sourceByRegex ./. ["package.*"];
+      toolchain = pkgs.rust-bin.nightly."2023-03-31".default;
+
+      naersk' = callPackage naersk {
+        rustc = toolchain;
+        cargo = toolchain;
+      };
     in rec {
-      # `nix develop`
-      devShell = pkgs.mkShell {
+      packages = rec {
+        node_modules = pkgs.npmlock2nix.v2.node_modules {
+          src = nodeSrc;
+        };
+        frontend = naersk'.buildPackage {
+          pname = "demostf-frontend";
+          root = src;
+
+          preBuild = ''
+            ln -s ${node_modules}/node_modules .
+          '';
+          nativeBuildInputs = with pkgs; [pkg-config openssl];
+        };
+      };
+      devShells.default = pkgs.mkShell {
         OPENSSL_NO_VENDOR = 1;
 
         nativeBuildInputs = with pkgs; [
-          rust-bin.nightly."2023-03-31".default
+          toolchain
           bacon
           cargo-edit
           cargo-outdated
@@ -34,6 +68,7 @@
           nodejs
           nodePackages.svgo
           typescript
+          sqlx-cli
         ];
       };
     });
